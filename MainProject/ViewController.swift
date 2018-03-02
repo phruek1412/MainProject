dbcccf7bee85e897613f72db5a8ad9597c511836
Phruek <phruek1412@gmail.com>
2018-03-02 16:18:53 +0700

    Initial commit

---
+++ MainProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MainProject
+//
+//  Created by Phruek Kosonsuwiwat on 2/3/2561 BE.
+//  Copyright © 2561 Phruek Kosonsuwiwat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
